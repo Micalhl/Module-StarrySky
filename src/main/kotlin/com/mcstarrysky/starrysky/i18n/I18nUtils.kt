ba6731c1e5c31d10649bf084e19ef813043c8cb0
Micalhl <micalhl@sina.cn>
2023-08-30 20:49:52 +0800

    [1.0.12][publish][Experimental] Update Comments (recommend: 1.0.11-9)

--- src/main/kotlin/com/mcstarrysky/starrysky/i18n/I18nUtils.kt
+++ src/main/kotlin/com/mcstarrysky/starrysky/i18n/I18nUtils.kt
@@ -10,14 +10,33 @@ import taboolib.common.platform.function.adaptPlayer
 import taboolib.common.util.VariableReader
 import taboolib.module.chat.ComponentText
 
+/** sendLang */
 fun Player.sendLang(node: String, vararg args: Pair<String, Any>, prefix: Boolean = true) {
     I18n.getLocaleFile(adaptPlayer(this)).send(this, node, *args, prefix = prefix)
 }
 
+fun CommandSender.sendLang(node: String, vararg args: Pair<String, Any>, prefix: Boolean = true) {
+    I18n.getLocaleFile(adaptCommandSender(this)).send(adaptCommandSender(this), node, *args, prefix = prefix)
+}
+
+fun ProxyCommandSender.sendLang(node: String, vararg args: Pair<String, Any>, prefix: Boolean = true) {
+    I18n.getLocaleFile(this).send(this, node, *args, prefix = prefix)
+}
+
+/** sendRaw */
 fun Player.sendRaw(msg: String, vararg args: Pair<String, Any>, prefix: Boolean = true) {
     I18n.getLocaleFile(adaptPlayer(this)).sendRaw(this, msg, *args, prefix = prefix)
 }
 
+fun CommandSender.sendRaw(msg: String, vararg args: Pair<String, Any>, prefix: Boolean = true) {
+    I18n.getLocaleFile(adaptCommandSender(this)).sendRaw(adaptCommandSender(this), msg, *args, prefix = prefix)
+}
+
+fun ProxyCommandSender.sendRaw(msg: String, vararg args: Pair<String, Any>, prefix: Boolean = true) {
+    I18n.getLocaleFile(this).sendRaw(this, msg, *args, prefix = prefix)
+}
+
+/** asLangText */
 fun Player.asLangText(node: String, vararg args: Pair<String, Any>, prefix: Boolean = false): ComponentText {
     return with(I18n.getLocaleFile(adaptPlayer(this))) {
         if (prefix) {
@@ -28,20 +47,14 @@ fun Player.asLangText(node: String, vararg args: Pair<String, Any>, prefix: Bool
     }
 }
 
-fun CommandSender.sendLang(node: String, vararg args: Pair<String, Any>, prefix: Boolean = true) {
-    I18n.getLocaleFile(adaptCommandSender(this)).send(adaptCommandSender(this), node, *args, prefix = prefix)
-}
-
-fun CommandSender.sendRaw(msg: String, vararg args: Pair<String, Any>, prefix: Boolean = true) {
-    I18n.getLocaleFile(adaptCommandSender(this)).sendRaw(adaptCommandSender(this), msg, *args, prefix = prefix)
-}
-
-fun ProxyCommandSender.sendLang(node: String, vararg args: Pair<String, Any>, prefix: Boolean = true) {
-    I18n.getLocaleFile(this).send(this, node, *args, prefix = prefix)
-}
-
-fun ProxyCommandSender.sendRaw(msg: String, vararg args: Pair<String, Any>, prefix: Boolean = true) {
-    I18n.getLocaleFile(this).sendRaw(this, msg, *args, prefix = prefix)
+fun CommandSender.asLangText(node: String, vararg args: Pair<String, Any>, prefix: Boolean = false): ComponentText {
+    return with(I18n.getLocaleFile(adaptCommandSender(this))) {
+        if (prefix) {
+            cacheWithPrefix(node, *args)
+        } else {
+            cache(node, *args)
+        }
+    }
 }
 
 fun ProxyCommandSender.asLangText(node: String, vararg args: Pair<String, Any>, prefix: Boolean = false): ComponentText {
@@ -54,8 +67,38 @@ fun ProxyCommandSender.asLangText(node: String, vararg args: Pair<String, Any>, 
     }
 }
 
+/** asLangTextString 获取原始行内复合文本 */
+fun Player.asLangTextString(node: String, vararg args: Pair<String, Any>): String {
+    return I18n.getLocaleFile(adaptPlayer(this)).origin(node, *args, player = this)
+}
+
+fun CommandSender.asLangTextString(node: String, vararg args: Pair<String, Any>): String {
+    return I18n.getLocaleFile(adaptCommandSender(this)).origin(node, *args)
+}
+
 fun ProxyCommandSender.asLangTextString(node: String, vararg args: Pair<String, Any>): String {
     return I18n.getLocaleFile(this).origin(node, *args, player = this.castSafely<CommandSender>() as? Player)
+}
+
+/** sendLangVariables 替换 List 变量 */
+fun Player.sendLangVariables(node: String, vararg args: Pair<String, Any>, reader: VariableReader = VariableReaders.BRACES, prefix: Boolean = true, variables: VariableFunction) {
+    with(I18n.getLocaleFile(adaptPlayer(this))) {
+        if (prefix) {
+            variablesWithPrefix(node, reader = reader, args = args, variables = variables).sendTo(adaptPlayer(this@sendLangVariables))
+        } else {
+            variables(node, reader = reader, args = args, variables = variables).sendTo(adaptPlayer(this@sendLangVariables))
+        }
+    }
+}
+
+fun CommandSender.sendLangVariables(node: String, vararg args: Pair<String, Any>, reader: VariableReader = VariableReaders.BRACES, prefix: Boolean = true, variables: VariableFunction) {
+    with(I18n.getLocaleFile(adaptCommandSender(this))) {
+        if (prefix) {
+            variablesWithPrefix(node, reader = reader, args = args, variables = variables).sendTo(adaptCommandSender(this@sendLangVariables))
+        } else {
+            variables(node, reader = reader, args = args, variables = variables).sendTo(adaptCommandSender(this@sendLangVariables))
+        }
+    }
 }
 
 fun ProxyCommandSender.sendLangVariables(node: String, vararg args: Pair<String, Any>, reader: VariableReader = VariableReaders.BRACES, prefix: Boolean = true, variables: VariableFunction) {
